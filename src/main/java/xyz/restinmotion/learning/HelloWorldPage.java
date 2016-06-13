24d66b4ddaa86e16d6ed13b47b3ae8f5cc86b1c6
efim <efim666@bk.ru>
2016-06-13 23:08:49 +0300

    anonimous link from HelloWorldPage to HomePage

--- src/main/java/xyz/restinmotion/learning/HelloWorldPage.java
+++ src/main/java/xyz/restinmotion/learning/HelloWorldPage.java
@@ -2,11 +2,20 @@ package xyz.restinmotion.learning;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HelloWorldPage extends WebPage {
 	public HelloWorldPage() {
 		super();
+
+		Link homePageLink = new Link("home_page_link") {
+			@Override
+			public void onClick() {
+				this.setResponsePage(new HomePage());
+			}
+		};
+		this.add(homePageLink);
 
 		add(new Label("message", "Hello World!"));
 
--- src/main/java/xyz/restinmotion/learning/HomePage.java
+++ src/main/java/xyz/restinmotion/learning/HomePage.java
@@ -9,8 +9,8 @@ import java.util.Date;
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
-	public HomePage(final PageParameters parameters) {
-		super(parameters);
+	public HomePage() {
+		super();
 
 		//label with version
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
