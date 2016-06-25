a26436b27fab09189ac7ea22f7533979271b8254
efim <efim666@bk.ru>
2016-06-25 12:54:45 +0300

    using PropertyModel for simple field

--- src/main/java/xyz/restinmotion/learning/HomePage.java
+++ src/main/java/xyz/restinmotion/learning/HomePage.java
@@ -5,28 +5,31 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import java.util.Date;
 
 public class HomePage extends BasePage {
 	private static final long serialVersionUID = 1L;
+	private String username;
+
 
 	public HomePage() {
 		super();
 
 		this.add(new FeedbackPanel("feedback"));
 
-		final TextField<String> username = new TextField<>("username",
-				Model.of(""));
-		username.setRequired(true);
-		username.add(new UsernameValidator());
+		final TextField<String> tUsername = new TextField<>("username",
+				new PropertyModel(this, "username"));
+		tUsername.setRequired(true);
+		tUsername.add(new UsernameValidator());
 
 		Form<?> form = new Form<Void>("userForm") {
 
 			@Override
 			protected void onSubmit() {
-				final String usernameValue = username.getModelObject();
+				final String usernameValue = username;//tUsername.getModelObject();
 
 				PageParameters pageParameters = new PageParameters();
 				pageParameters.add("username", usernameValue);
@@ -35,7 +38,7 @@ public class HomePage extends BasePage {
 		};
 
 		add(form);
-		form.add(username);
+		form.add(tUsername);
 
 		//label with version
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
